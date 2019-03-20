f7452c666af01ca2216abb0560e7dad73942c9bf
abidmmp <abidmmp99@gmail.com>
2019-03-20 09:00:00 +0500

    Implement useAsync functionality

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -53,3 +53,25 @@ const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDiv
 CardFooter.displayName = "CardFooter";
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent };
+
+
+import { useState, useCallback } from 'react';
+
+interface AsyncState<T> { data: T | null; loading: boolean; error: string | null; }
+
+export function useAsync<T>() {
+  const [state, setState] = useState<AsyncState<T>>({ data: null, loading: false, error: null });
+  const execute = useCallback(async (fn: () => Promise<T>) => {
+    setState({ data: null, loading: true, error: null });
+    try {
+      const data = await fn();
+      setState({ data, loading: false, error: null });
+      return data;
+    } catch (e) {
+      const error = e instanceof Error ? e.message : 'Unknown error';
+      setState({ data: null, loading: false, error });
+      throw e;
+    }
+  }, []);
+  return { ...state, execute };
+}
