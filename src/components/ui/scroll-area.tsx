ea03857313e33ecdcf3b1c7219de5c74e02db47f
abidmmp <abidmmp99@gmail.com>
2019-05-11 10:17:00 +0500

    Optimize useLocalStorage performance

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -42,3 +42,21 @@ const ScrollBar = React.forwardRef<
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
 
 export { ScrollArea, ScrollBar };
+
+
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage<T>(key: string, initial: T) {
+  const [value, setValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initial;
+    } catch {
+      return initial;
+    }
+  });
+  useEffect(() => {
+    try { localStorage.setItem(key, JSON.stringify(value)); } catch {}
+  }, [key, value]);
+  return [value, setValue] as const;
+}
