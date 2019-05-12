9d43df6acd576230ee80f0eb74fe8746c64e917a
abidmmp <abidmmp99@gmail.com>
2019-05-12 09:00:00 +0500

    Implement useClickOutside functionality

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -60,3 +60,20 @@ export function useLocalStorage<T>(key: string, initial: T) {
   }, [key, value]);
   return [value, setValue] as const;
 }
+
+
+import { useEffect, useRef, RefObject } from 'react';
+
+export function useClickOutside<T extends HTMLElement>(
+  handler: () => void
+): RefObject<T> {
+  const ref = useRef<T>(null);
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) handler();
+    };
+    document.addEventListener('mousedown', listener);
+    return () => document.removeEventListener('mousedown', listener);
+  }, [handler]);
+  return ref;
+}
