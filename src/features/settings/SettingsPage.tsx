6e685c532c2b1c5295fe175838007320e65fc3f4
abidmmp <abidmmp99@gmail.com>
2019-06-15 11:34:00 +0500

    Implement useClickOutside functionality

--- src/features/settings/SettingsPage.tsx
+++ src/features/settings/SettingsPage.tsx
@@ -173,3 +173,20 @@ function Row({
     </div>
   );
 }
+
+
+import { useEffect, useRef, RefObject } from 'react';
+
+export function useClickOutside<T extends HTMLElement>(
+  handler: () => void
+): RefObject<T> {
+  const ref = useRef<T>(null);
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) handler();
+    };
+    document.addEventListener('mousedown', listener);
+    return () => document.removeEventListener('mousedown', listener);
+  }, [handler]);
+  return ref;
+}
