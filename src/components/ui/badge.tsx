b095ba47023cebbac1be30427ca015bfb6a5dac3
abidmmp <abidmmp99@gmail.com>
2019-06-15 10:17:00 +0500

    Optimize useLocalStorage performance

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -30,3 +30,21 @@ function Badge({ className, variant, ...props }: BadgeProps) {
 }
 
 export { Badge, badgeVariants };
+
+
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage<T>(key: string, initial: T) {
+  const [value, setValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initial;
+    } catch {
+      return initial;
+    }
+  });
+  useEffect(() => {
+    try { localStorage.setItem(key, JSON.stringify(value)); } catch {}
+  }, [key, value]);
+  return [value, setValue] as const;
+}
