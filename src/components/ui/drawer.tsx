81f70c5371b637cb1089d969157fdbe32f178e1f
abidmmp <abidmmp99@gmail.com>
2019-06-16 09:00:00 +0500

    Implement useToggle functionality

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -96,3 +96,21 @@ export {
   DrawerTitle,
   DrawerDescription,
 };
+
+
+import { useState, useCallback } from 'react';
+
+interface UseToggleReturn {
+  value: boolean;
+  toggle: () => void;
+  setTrue: () => void;
+  setFalse: () => void;
+}
+
+export function useToggle(initial = false): UseToggleReturn {
+  const [value, setValue] = useState(initial);
+  const toggle   = useCallback(() => setValue(v => !v), []);
+  const setTrue  = useCallback(() => setValue(true), []);
+  const setFalse = useCallback(() => setValue(false), []);
+  return { value, toggle, setTrue, setFalse };
+}
