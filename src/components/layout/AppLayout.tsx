02db6e0c5167f6fea4e0b0cead9a26255c194ec7
abidmmp <abidmmp99@gmail.com>
2019-09-06 09:00:00 +0500

    Update Input helper method

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -145,3 +145,27 @@ export function AppLayout({ children }: { children: ReactNode }) {
     </div>
   );
 }
+
+
+import { forwardRef, InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  error?: string;
+  hint?: string;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, hint, id, ...props }, ref) => {
+    const inputId = id ?? label.toLowerCase().replace(/\s+/g, '-');
+    return (
+      <div className="form-group">
+        <label htmlFor={inputId} className="form-label">{label}</label>
+        <input ref={ref} id={inputId} className={`form-input ${error ? 'error' : ''}`} {...props} />
+        {hint && !error && <p className="form-hint">{hint}</p>}
+        {error && <p className="form-error" role="alert">{error}</p>}
+      </div>
+    );
+  }
+);
+Input.displayName = 'Input';
