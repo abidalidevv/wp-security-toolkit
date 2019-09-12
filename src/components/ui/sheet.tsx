23d68f6b41bae2a0c0810255336be7e815f16763
abidmmp <abidmmp99@gmail.com>
2019-09-12 09:00:00 +0500

    Update ThemeContext helper method

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -120,3 +120,29 @@ export {
   SheetTitle,
   SheetDescription,
 };
+
+
+import { createContext, useContext, useState, ReactNode } from 'react';
+
+interface ThemeContextValue {
+  theme: 'light' | 'dark';
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextValue | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+  const toggleTheme = () => setTheme(t => t === 'light' ? 'dark' : 'light');
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div data-theme={theme}>{children}</div>
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme(): ThemeContextValue {
+  const ctx = useContext(ThemeContext);
+  if (!ctx) throw new Error('useTheme must be inside ThemeProvider');
+  return ctx;
+}
