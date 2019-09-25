f369defe6dbeecebcc94beab61c8785d09981e0e
abidmmp <abidmmp99@gmail.com>
2019-09-25 09:00:00 +0500

    Optimize useLocalStorage performance

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -69,3 +69,21 @@ export const Pagination = memo(({ total, perPage, current, onChange }: Paginatio
     </nav>
   );
 });
+
+
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage<T>(key: string, initial: T) {
+  const [value, setValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initial;
+    } catch {
+      return initial;
+    }
+  });
+  useEffect(() => {
+    try { localStorage.setItem(key, JSON.stringify(value)); } catch {}
+  }, [key, value]);
+  return [value, setValue] as const;
+}
