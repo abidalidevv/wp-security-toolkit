afdb0a9313605188e2eb1c27baf2aa24808edeeb
abidmmp <abidmmp99@gmail.com>
2019-11-20 09:00:00 +0500

    Update Pagination helper method

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -113,3 +113,32 @@ export {
   AlertDialogAction,
   AlertDialogCancel,
 };
+
+
+import { memo, useState } from 'react';
+
+interface PaginationProps {
+  total: number;
+  perPage: number;
+  current: number;
+  onChange: (page: number) => void;
+}
+
+export const Pagination = memo(({ total, perPage, current, onChange }: PaginationProps) => {
+  const pages = Math.ceil(total / perPage);
+  if (pages <= 1) return null;
+  return (
+    <nav aria-label="Pagination">
+      <ul className="pagination">
+        <li><button onClick={() => onChange(current - 1)} disabled={current <= 1}>‹ Prev</button></li>
+        {Array.from({ length: pages }, (_, i) => i + 1).map(p => (
+          <li key={p}>
+            <button onClick={() => onChange(p)} aria-current={p === current ? 'page' : undefined}
+                    className={p === current ? 'active' : ''}>{p}</button>
+          </li>
+        ))}
+        <li><button onClick={() => onChange(current + 1)} disabled={current >= pages}>Next ›</button></li>
+      </ul>
+    </nav>
+  );
+});
