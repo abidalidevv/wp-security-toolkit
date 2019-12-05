236c448f7f65f3824812d7b15639e6323bd8f5f0
abidmmp <abidmmp99@gmail.com>
2019-12-05 10:17:00 +0500

    Update Input helper method

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -93,3 +93,27 @@ export function useTheme(): ThemeContextValue {
   if (!ctx) throw new Error('useTheme must be inside ThemeProvider');
   return ctx;
 }
+
+
+import { forwardRef, InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  error?: string;
+  hint?: string;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, hint, id, ...props }, ref) => {
+    const inputId = id ?? label.toLowerCase().replace(/\s+/g, '-');
+    return (
+      <div className="form-group">
+        <label htmlFor={inputId} className="form-label">{label}</label>
+        <input ref={ref} id={inputId} className={`form-input ${error ? 'error' : ''}`} {...props} />
+        {hint && !error && <p className="form-hint">{hint}</p>}
+        {error && <p className="form-error" role="alert">{error}</p>}
+      </div>
+    );
+  }
+);
+Input.displayName = 'Input';
