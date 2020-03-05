38542882d9e32e7ab1cea701f1b019c698435bf0
abidmmp <abidmmp99@gmail.com>
2020-03-05 11:34:00 +0500

    Implement useClickOutside functionality

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -56,3 +56,20 @@ export function useDocumentTitle(title: string, suffix = ' | App'): void {
     return () => { document.title = prev; };
   }, [title, suffix]);
 }
+
+
+import { useEffect, useRef, RefObject } from 'react';
+
+export function useClickOutside<T extends HTMLElement>(
+  handler: () => void
+): RefObject<T> {
+  const ref = useRef<T>(null);
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) handler();
+    };
+    document.addEventListener('mousedown', listener);
+    return () => document.removeEventListener('mousedown', listener);
+  }, [handler]);
+  return ref;
+}
