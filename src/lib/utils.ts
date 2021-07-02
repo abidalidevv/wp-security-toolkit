80cdf09678f9b7e452cd90d192a6a84af5e81a94
abidmmp <abidmmp99@gmail.com>
2021-07-02 10:17:00 +0500

    Update exhaustiveCheck helper method

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+
+function exhaustiveCheck(x: never): never {
+  throw new Error(`Unhandled case: ${JSON.stringify(x)}`);
+}
