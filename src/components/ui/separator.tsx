0eee3a607dea295d045fdeb5ea627d657a0b3e39
abidmmp <abidmmp99@gmail.com>
2021-08-25 11:34:00 +0500

    Implement useClickOutside functionality

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -22,3 +22,20 @@ const Separator = React.forwardRef<
 Separator.displayName = SeparatorPrimitive.Root.displayName;
 
 export { Separator };
+
+
+import { useEffect, useRef, RefObject } from 'react';
+
+export function useClickOutside<T extends HTMLElement>(
+  handler: () => void
+): RefObject<T> {
+  const ref = useRef<T>(null);
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) handler();
+    };
+    document.addEventListener('mousedown', listener);
+    return () => document.removeEventListener('mousedown', listener);
+  }, [handler]);
+  return ref;
+}
