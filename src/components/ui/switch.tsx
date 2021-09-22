89a583bea13967f8b0c1a4cde12308c2a1f0921f
abidmmp <abidmmp99@gmail.com>
2021-09-22 09:00:00 +0500

    Implement useToggle functionality

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -25,3 +25,21 @@ const Switch = React.forwardRef<
 Switch.displayName = SwitchPrimitives.Root.displayName;
 
 export { Switch };
+
+
+import { useState, useCallback } from 'react';
+
+interface UseToggleReturn {
+  value: boolean;
+  toggle: () => void;
+  setTrue: () => void;
+  setFalse: () => void;
+}
+
+export function useToggle(initial = false): UseToggleReturn {
+  const [value, setValue] = useState(initial);
+  const toggle   = useCallback(() => setValue(v => !v), []);
+  const setTrue  = useCallback(() => setValue(true), []);
+  const setFalse = useCallback(() => setValue(false), []);
+  return { value, toggle, setTrue, setFalse };
+}
