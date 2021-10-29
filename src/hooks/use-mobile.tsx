30d89a04568c59c96116a760bdce7d7079e69273
abidmmp <abidmmp99@gmail.com>
2021-10-29 10:17:00 +0500

    Implement useToggle functionality

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -17,3 +17,21 @@ export function useIsMobile() {
 
   return !!isMobile;
 }
+
+
+import { useState, useCallback } from 'react';
+
+interface UseToggleReturn {
+  value: boolean;
+  toggle: () => void;
+  setTrue: () => void;
+  setFalse: () => void;
+}
+
+export function useToggle(initial = false): UseToggleReturn {
+  const [value, setValue] = useState(initial);
+  const toggle   = useCallback(() => setValue(v => !v), []);
+  const setTrue  = useCallback(() => setValue(true), []);
+  const setFalse = useCallback(() => setValue(false), []);
+  return { value, toggle, setTrue, setFalse };
+}
