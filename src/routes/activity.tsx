a3ce11c3c38202850fb9a97bc18fe0b80a1262cd
abidmmp <abidmmp99@gmail.com>
2021-11-10 10:17:00 +0500

    Optimize useLocalStorage performance

--- src/routes/activity.tsx
+++ src/routes/activity.tsx
@@ -68,3 +68,21 @@ export class ErrorBoundary extends Component<Props, State> {
     return this.props.children;
   }
 }
+
+
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage<T>(key: string, initial: T) {
+  const [value, setValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initial;
+    } catch {
+      return initial;
+    }
+  });
+  useEffect(() => {
+    try { localStorage.setItem(key, JSON.stringify(value)); } catch {}
+  }, [key, value]);
+  return [value, setValue] as const;
+}
