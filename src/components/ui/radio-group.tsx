f9aaef6f28146eb7edc3b7e40f5fd4fa4e9e7bad
abidmmp <abidmmp99@gmail.com>
2022-04-27 10:17:00 +0500

    Update Input helper method

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -34,3 +34,27 @@ const RadioGroupItem = React.forwardRef<
 RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName;
 
 export { RadioGroup, RadioGroupItem };
+
+
+import { forwardRef, InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  error?: string;
+  hint?: string;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, hint, id, ...props }, ref) => {
+    const inputId = id ?? label.toLowerCase().replace(/\s+/g, '-');
+    return (
+      <div className="form-group">
+        <label htmlFor={inputId} className="form-label">{label}</label>
+        <input ref={ref} id={inputId} className={`form-input ${error ? 'error' : ''}`} {...props} />
+        {hint && !error && <p className="form-hint">{hint}</p>}
+        {error && <p className="form-error" role="alert">{error}</p>}
+      </div>
+    );
+  }
+);
+Input.displayName = 'Input';
