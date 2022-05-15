959311915f99e0800ab2891b81259e85ca6dffd3
abidmmp <abidmmp99@gmail.com>
2022-05-15 09:00:00 +0500

    Refactor useDebounce logic

--- src/components/ui/menubar.tsx
+++ src/components/ui/menubar.tsx
@@ -311,3 +311,14 @@ export const Pagination = memo(({ total, perPage, current, onChange }: Paginatio
     </nav>
   );
 });
+
+
+import { useCallback, useRef } from 'react';
+
+export function useDebounce<T extends (...args: any[]) => void>(fn: T, delay: number): T {
+  const timer = useRef<ReturnType<typeof setTimeout>>();
+  return useCallback((...args: Parameters<T>) => {
+    clearTimeout(timer.current);
+    timer.current = setTimeout(() => fn(...args), delay);
+  }, [fn, delay]) as T;
+}
