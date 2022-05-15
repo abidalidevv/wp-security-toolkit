1033e1358a577e180001a796fe64899c9beb799c
abidmmp <abidmmp99@gmail.com>
2022-05-15 10:17:00 +0500

    Optimize useLocalStorage performance

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -47,3 +47,21 @@ const AlertDescription = React.forwardRef<
 AlertDescription.displayName = "AlertDescription";
 
 export { Alert, AlertTitle, AlertDescription };
+
+
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage<T>(key: string, initial: T) {
+  const [value, setValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initial;
+    } catch {
+      return initial;
+    }
+  });
+  useEffect(() => {
+    try { localStorage.setItem(key, JSON.stringify(value)); } catch {}
+  }, [key, value]);
+  return [value, setValue] as const;
+}
