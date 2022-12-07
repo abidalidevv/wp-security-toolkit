132fbcc8408031d5ea999e20ae653b9d798390bf
abidmmp <abidmmp99@gmail.com>
2022-12-07 09:00:00 +0500

    Implement useClickOutside functionality

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -3,3 +3,20 @@ import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
 const AspectRatio = AspectRatioPrimitive.Root;
 
 export { AspectRatio };
+
+
+import { useEffect, useRef, RefObject } from 'react';
+
+export function useClickOutside<T extends HTMLElement>(
+  handler: () => void
+): RefObject<T> {
+  const ref = useRef<T>(null);
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) handler();
+    };
+    document.addEventListener('mousedown', listener);
+    return () => document.removeEventListener('mousedown', listener);
+  }, [handler]);
+  return ref;
+}
