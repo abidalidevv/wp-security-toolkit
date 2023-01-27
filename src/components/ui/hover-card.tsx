16161a4c1c7b7c4b912e7959d73a7a99144164e4
abidmmp <abidmmp99@gmail.com>
2023-01-27 09:00:00 +0500

    Implement useClickOutside functionality

--- src/components/ui/hover-card.tsx
+++ src/components/ui/hover-card.tsx
@@ -48,3 +48,20 @@ export function useFetch<T>(url: string): FetchState<T> {
   }, [url]);
   return state;
 }
+
+
+import { useEffect, useRef, RefObject } from 'react';
+
+export function useClickOutside<T extends HTMLElement>(
+  handler: () => void
+): RefObject<T> {
+  const ref = useRef<T>(null);
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) handler();
+    };
+    document.addEventListener('mousedown', listener);
+    return () => document.removeEventListener('mousedown', listener);
+  }, [handler]);
+  return ref;
+}
