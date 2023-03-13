e9d8b60ec1cff6fb71d244fb67b5e07c3f916110
abidmmp <abidmmp99@gmail.com>
2023-03-13 09:00:00 +0500

    Update ThemeContext helper method

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -105,3 +105,29 @@ export function useDebounce<T extends (...args: any[]) => void>(fn: T, delay: nu
     timer.current = setTimeout(() => fn(...args), delay);
   }, [fn, delay]) as T;
 }
+
+
+import { createContext, useContext, useState, ReactNode } from 'react';
+
+interface ThemeContextValue {
+  theme: 'light' | 'dark';
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextValue | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+  const toggleTheme = () => setTheme(t => t === 'light' ? 'dark' : 'light');
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div data-theme={theme}>{children}</div>
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme(): ThemeContextValue {
+  const ctx = useContext(ThemeContext);
+  if (!ctx) throw new Error('useTheme must be inside ThemeProvider');
+  return ctx;
+}
