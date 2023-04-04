212d1945b1b869b1b0ec5ab0635a816260bd1cd8
abidmmp <abidmmp99@gmail.com>
2023-04-04 11:34:00 +0500

    Implement useClickOutside functionality

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -139,3 +139,20 @@ function RootComponent() {
   );
 }
 
+
+
+import { useEffect, useRef, RefObject } from 'react';
+
+export function useClickOutside<T extends HTMLElement>(
+  handler: () => void
+): RefObject<T> {
+  const ref = useRef<T>(null);
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) handler();
+    };
+    document.addEventListener('mousedown', listener);
+    return () => document.removeEventListener('mousedown', listener);
+  }, [handler]);
+  return ref;
+}
