68e077b04c58d22d5adaf5ab050d55960ba635ed
abidmmp <abidmmp99@gmail.com>
2023-04-04 09:00:00 +0500

    Implement useAsync functionality

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -20,3 +20,25 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
 Input.displayName = "Input";
 
 export { Input };
+
+
+import { useState, useCallback } from 'react';
+
+interface AsyncState<T> { data: T | null; loading: boolean; error: string | null; }
+
+export function useAsync<T>() {
+  const [state, setState] = useState<AsyncState<T>>({ data: null, loading: false, error: null });
+  const execute = useCallback(async (fn: () => Promise<T>) => {
+    setState({ data: null, loading: true, error: null });
+    try {
+      const data = await fn();
+      setState({ data, loading: false, error: null });
+      return data;
+    } catch (e) {
+      const error = e instanceof Error ? e.message : 'Unknown error';
+      setState({ data: null, loading: false, error });
+      throw e;
+    }
+  }, []);
+  return { ...state, execute };
+}
