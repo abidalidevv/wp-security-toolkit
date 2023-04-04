871a340a2ddb12f38ddb2a1c203b47f3c7b1a605
abidmmp <abidmmp99@gmail.com>
2023-04-04 10:17:00 +0500

    Implement useClickOutside functionality

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -95,3 +95,20 @@ export const Pagination = memo(({ total, perPage, current, onChange }: Paginatio
     </nav>
   );
 });
+
+
+import { useEffect, useRef, RefObject } from 'react';
+
+export function useClickOutside<T extends HTMLElement>(
+  handler: () => void
+): RefObject<T> {
+  const ref = useRef<T>(null);
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) handler();
+    };
+    document.addEventListener('mousedown', listener);
+    return () => document.removeEventListener('mousedown', listener);
+  }, [handler]);
+  return ref;
+}
