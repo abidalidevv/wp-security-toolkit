c306d6b7e50233f1c2362094e17d5b6ce1e90a38
abidmmp <abidmmp99@gmail.com>
2023-06-16 10:17:00 +0500

    Update ThemeContext helper method

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -95,3 +95,29 @@ export function useLocalStorage<T>(key: string, initial: T) {
   }, [key, value]);
   return [value, setValue] as const;
 }
+
+
+import { createContext, useContext, useState, ReactNode } from 'react';
+
+interface ThemeContextValue {
+  theme: 'light' | 'dark';
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextValue | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+  const toggleTheme = () => setTheme(t => t === 'light' ? 'dark' : 'light');
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div data-theme={theme}>{children}</div>
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme(): ThemeContextValue {
+  const ctx = useContext(ThemeContext);
+  if (!ctx) throw new Error('useTheme must be inside ThemeProvider');
+  return ctx;
+}
