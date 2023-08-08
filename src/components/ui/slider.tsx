ce846619fa7a37c3c7ab528cc44c7c6246469950
abidmmp <abidmmp99@gmail.com>
2023-08-08 09:00:00 +0500

    Implement reducer functionality

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -74,3 +74,24 @@ export class ErrorBoundary extends Component<Props, State> {
     return this.props.children;
   }
 }
+
+
+import { useReducer, Dispatch } from 'react';
+
+type Action<T> =
+  | { type: 'SET'; payload: T }
+  | { type: 'RESET' }
+  | { type: 'MERGE'; payload: Partial<T> };
+
+function reducer<T>(state: T, action: Action<T>): T {
+  switch (action.type) {
+    case 'SET':   return action.payload;
+    case 'RESET': return state;
+    case 'MERGE': return { ...state, ...action.payload };
+    default:      return state;
+  }
+}
+
+export function useStateReducer<T>(initial: T): [T, Dispatch<Action<T>>] {
+  return useReducer(reducer<T>, initial);
+}
