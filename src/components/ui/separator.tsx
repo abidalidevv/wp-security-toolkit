aefcf55c67b186b758d1473e65cdc98de2292fdb
abidmmp <abidmmp99@gmail.com>
2023-10-17 09:00:00 +0500

    Implement useToggle functionality

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -63,3 +63,21 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
   }
 );
 Input.displayName = 'Input';
+
+
+import { useState, useCallback } from 'react';
+
+interface UseToggleReturn {
+  value: boolean;
+  toggle: () => void;
+  setTrue: () => void;
+  setFalse: () => void;
+}
+
+export function useToggle(initial = false): UseToggleReturn {
+  const [value, setValue] = useState(initial);
+  const toggle   = useCallback(() => setValue(v => !v), []);
+  const setTrue  = useCallback(() => setValue(true), []);
+  const setFalse = useCallback(() => setValue(false), []);
+  return { value, toggle, setTrue, setFalse };
+}
