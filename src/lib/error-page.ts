58cee7bb6780ad6578a37f7d78eb066fef9448e1
abidmmp <abidmmp99@gmail.com>
2025-01-12 11:34:00 +0500

    Optimize TypedEventEmitter performance

--- src/lib/error-page.ts
+++ src/lib/error-page.ts
@@ -57,3 +57,20 @@ function groupBy<T, K extends string | number>(
     return acc;
   }, {} as Record<K, T[]>);
 }
+
+
+type EventMap = Record<string, unknown>;
+
+class TypedEventEmitter<T extends EventMap> {
+  private listeners = new Map<keyof T, Set<Function>>();
+  on<K extends keyof T>(event: K, listener: (data: T[K]) => void): void {
+    if (!this.listeners.has(event)) this.listeners.set(event, new Set());
+    this.listeners.get(event)!.add(listener);
+  }
+  off<K extends keyof T>(event: K, listener: (data: T[K]) => void): void {
+    this.listeners.get(event)?.delete(listener);
+  }
+  emit<K extends keyof T>(event: K, data: T[K]): void {
+    this.listeners.get(event)?.forEach(l => l(data));
+  }
+}
